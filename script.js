38280375e7832ddb2462a0755b9f46c83f84a9c1
rushbagul5566-sudo <rushbagul5566@gmail.com>
2026-07-24 10:25:43 +0530

    Add welcome alert on button click in script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".hero button");
 
+    button.addEventListener("click", () => {
+        alert("Welcome to Sky Healthcare!\nOnline Booking feature will be available soon.");
+    });
+});
