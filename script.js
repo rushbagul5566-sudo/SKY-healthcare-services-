3b9046bf92fb2f3b1fd982642dc8ded5402523bb
rushbagul5566-sudo <rushbagul5566@gmail.com>
2026-07-24 12:17:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
         alert("Welcome to Sky Healthcare!\nOnline Booking feature will be available soon.");
     });
 });
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCNWjruHTgBCz8YGXCP1R2P7m2mLs1i1mI",
+  authDomain: "skyhealthcare-76db5.firebaseapp.com",
+  projectId: "skyhealthcare-76db5",
+  storageBucket: "skyhealthcare-76db5.firebasestorage.app",
+  messagingSenderId: "341680101155",
+  appId: "1:341680101155:web:62b8b496d21d2fadc71e96",
+  measurementId: "G-DGRPR5QEM0"
+};
