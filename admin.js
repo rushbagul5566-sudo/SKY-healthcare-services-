28b9c459cdb7180ee4333c00882f4e7aec9f4245
rushbagul5566-sudo <rushbagul5566@gmail.com>
2026-07-24 13:26:06 +0530

    Add Firebase integration to fetch booking data

---
+++ admin.js
@@ -0,0 +1,35 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNWjruHTgBCz8YGXCP1R2P7m2mLs1i1mI",
+  authDomain: "skyhealthcare-76db5.firebaseapp.com",
+  projectId: "skyhealthcare-76db5",
+  storageBucket: "skyhealthcare-76db5.firebasestorage.app",
+  messagingSenderId: "341680101155",
+  appId: "1:341680101155:web:62b8b496d21d2fadc71e96",
+  measurementId: "G-DGRPR5QEM0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const bookingList = document.getElementById("bookingList");
+
+const querySnapshot = await getDocs(collection(db, "bookings"));
+
+querySnapshot.forEach((doc) => {
+
+    const data = doc.data();
+
+    bookingList.innerHTML += `
+        <tr>
+            <td>${data.name}</td>
+            <td>${data.mobile}</td>
+            <td>${data.service}</td>
+            <td>${data.address}</td>
+            <td>${data.date.toDate()}</td>
+        </tr>
+    `;
+
+});
